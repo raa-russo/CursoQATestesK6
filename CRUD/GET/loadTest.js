5da507fe53f97634377699a45a699d5f79d5a19e
russo <raa.augusto@hotmail.com>
2023-07-31 11:17:17 -0300

    load test teste de carga

--- CRUD/GET/loadTest.js
+++ CRUD/GET/loadTest.js
@@ -9,12 +9,17 @@ export function handleSummary(data) {
 }
 
 const baseUrl = "http://localhost:3000"; // URL base da Serverest
-const numberOfUsers = 10; // Número de usuários virtuais simulados
-const testDuration = "5s"; // Duração do teste de carga
 
 export const options = {
-  vus: numberOfUsers,
-  duration: testDuration,
+  stages: [
+    { duration: "1m", target: 100 }, // Aumenta gradativamente para o número máximo de usuários em 30 segundos
+    { duration: "2m", target: 100 }, // Mantém o número máximo de usuários por mais 30 segundos
+    { duration: "1m", target: 0 }, // Reduz gradualmente o número de usuários para 0 em 10 segundos
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<2000"], // Define o limite de 500 ms para a duração das requisições
+    http_req_failed: ['rate<0.01'] //1% das requisicoes podem falhar
+  },
 };
 
 export default function () {
@@ -22,17 +27,16 @@ export default function () {
     const endpoint = "/usuarios/";
     // Faz uma requisição GET para um endpoint da API Serverest
     const response = http.get(`${baseUrl}${endpoint}`);
-    try {      
-
+    try {  
       // Verifica se a resposta foi bem-sucedida (status 2xx)
       if (response.status >= 200 && response.status < 300) {
-        console.log('Load Test passou! API is up and running.');
+      //  console.log('Load Test passou! API is up and running.');
       } else {
-        console.error('Load Test falhou! API returned an error.');
+      //  console.error('Load Test falhou! API returned an error.');
       }
     } catch (error) {
-      console.error('Load Test falhou! Unable to connect to the API.');
-      //console.error(error.message);
+    //  console.error('Load Test falhou! Unable to connect to the API.');
+    //  console.error(error.message);
     }
 
     // Verifica se a resposta foi bem-sucedida (status 2xx)
