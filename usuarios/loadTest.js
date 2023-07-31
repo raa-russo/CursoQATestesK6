1f187f01eeacb1fafb45e358471da44cd4e1e4cb
russo <raa.augusto@hotmail.com>
2023-07-31 17:49:03 -0300

    teste de stress

--- usuarios/loadTest.js
+++ usuarios/loadTest.js
@@ -8,48 +8,48 @@ import { group, sleep } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export function handleSummary(data) {
-    return {
-      "summary.html": htmlReport(data),
-    };
-  }
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
 
-  export const options = {
-    stages: [
-      { duration: "1m", target: 100 },
+export const options = {
+  stages: [
+    { duration: "1m", target: 100 },
     { duration: "2m", target: 100 },
     { duration: "1m", target: 0 },
-      
-    ],
-    thresholds: {
-      http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'], 
-      http_req_failed: ['rate<0.01'] 
-    }
+
+  ],
+  thresholds: {
+    http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'],
+    http_req_failed: ['rate<0.01']
   }
+}
 
 export default () => {
-    group('Endpoint Login de usuários - Serverest.Api', () => {
-      Login();
-    });
+  group('Endpoint Login de usuários - Serverest.Api', () => {
+    Login();
+  });
 
-    group('Endpoint Listar usuários - Serverest.Api', () => {
-      ListarUsuarios();
-    });
+  group('Endpoint Listar usuários - Serverest.Api', () => {
+    ListarUsuarios();
+  });
 
-    group('Endpoint Editar usuários - Serverest.Api', () => {
-        EditarUsuarios();
-      });
+  group('Endpoint Editar usuários - Serverest.Api', () => {
+    EditarUsuarios();
+  });
 
-    group('Endpoint Cadastrar Usuário - Serverest.Api', () => {
-      CadastrarUsuarios();
-    });
+  group('Endpoint Cadastrar Usuário - Serverest.Api', () => {
+    CadastrarUsuarios();
+  });
 
-    group('Endpoint Listar Usuarios por id - Serverest.Api', () => {
-        ListarUsuariosId();
-    });
+  group('Endpoint Listar Usuarios por id - Serverest.Api', () => {
+    ListarUsuariosId();
+  });
 
-    group('Endpoint Excluir Usuários - Serverest.Api', () => {
-        ExcluirUsuarios();
-    });
+  group('Endpoint Excluir Usuários - Serverest.Api', () => {
+    ExcluirUsuarios();
+  });
 
-    sleep(1);
+  sleep(1);
 }
--- usuarios/stressTest.js
+++ usuarios/stressTest.js
@@ -1,6 +1,10 @@
-import http from "k6/http";
-import { sleep, check, fail } from "k6";
-import { Trend, Rate, Counter } from 'k6/metrics';
+import Login from "../login/login.js";
+import ListarUsuarios from "./listarUsers.js";
+import CadastrarUsuarios from "./cadastrarUsers.js";
+import ListarUsuariosId from "./listarUsersId.js";
+import EditarUsuarios from "./editarUsers.js";
+import ExcluirUsuarios from "./excluirUsers.js";
+import { group, sleep } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export function handleSummary(data) {
@@ -19,40 +23,38 @@ export const options = {
     { duration: "2s", target: 300 },
     { duration: "3s", target: 400 },
     { duration: "1s", target: 0 },
+
   ],
   thresholds: {
-    http_req_duration: ["p(95)<5000"],
-    http_req_failed: ['rate<0.05']
-  },
-};
-
-export let GetCustomerDuration = new Trend('get_customer_duration');
-export let GetCustomerFailRate = new Rate('get_customer_fail_rate');
-export let GetCustomerSuccessRate = new Rate('get_customer_success_rate');
-export let GetCustomerReqs = new Rate('get_customer_reqs');
-
-export default function () {
-  const url = 'http://localhost:3000/login';
-
-  const payload = JSON.stringify({
-    email: 'fulano@qa.com',
-    password: 'teste'
-  })
-  const headers = { 'headers': { 'Content-Type': 'application/json' } }
-  const res = http.post(url, payload, headers);
-
-  GetCustomerDuration.add(res.timings.duration);
-  GetCustomerReqs.add(1);
-  GetCustomerFailRate.add(res.status > 200 || res.status > 399);
-  GetCustomerSuccessRate.add(res.status < 399);
-
-  let durationMSG = 'Max Duration ${1000/1000}s'
-  if (!check(res, {
-    'max duration': (r) => r.timings.duration < 2000,
-    'is status code 200': (r) => r.status === 200,
-  })) {
-    fail(durationMSG);
+    http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'],
+    http_req_failed: ['rate<0.01']
   }
-  sleep(1);
 }
 
+export default () => {
+  group('Endpoint Login de usuários - Serverest.Api', () => {
+    Login();
+  });
+
+  group('Endpoint Listar usuários - Serverest.Api', () => {
+    ListarUsuarios();
+  });
+
+  group('Endpoint Editar usuários - Serverest.Api', () => {
+    EditarUsuarios();
+  });
+
+  group('Endpoint Cadastrar Usuário - Serverest.Api', () => {
+    CadastrarUsuarios();
+  });
+
+  group('Endpoint Listar Usuarios por id - Serverest.Api', () => {
+    ListarUsuariosId();
+  });
+
+  group('Endpoint Excluir Usuários - Serverest.Api', () => {
+    ExcluirUsuarios();
+  });
+
+  sleep(1);
+}
