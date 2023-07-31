5d96028db30da4f7b389248a92546d6d9e9b0752
russo <raa.augusto@hotmail.com>
2023-07-31 20:19:02 -0300

    cenario 04 usuario excluidor

---
+++ CENARIOS/cenario4.js
@@ -0,0 +1,38 @@
+import Login from "../login/login.js";
+import ExcluirUsuarios from "../usuarios/excluirUsers.js";
+import ExcluirProdutos from "../produtos/excluir.js";
+import { group, sleep } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }
+
+  export const options = {
+    stages: [
+      { duration: '1s', target: 1 },
+      
+    ],
+    thresholds: {
+      http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'], 
+      http_req_failed: ['rate<0.01'] 
+    }
+  }
+
+export default () => {
+    group('Endpoint Login de usuários - Serverest.Api', () => {
+      Login();
+    });
+
+    group('Endpoint Excluir usuários - Serverest.Api', () => {
+      ExcluirUsuarios();
+    });
+
+    group('Endpoint Excluir Produto - Serverest.Api', () => {
+      ExcluirProdutos();
+    });   
+
+    sleep(1);
+}
