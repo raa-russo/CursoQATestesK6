01d3a14c443a599cb822c8c646af100764e9f217
russo <raa.augusto@hotmail.com>
2023-07-31 12:31:50 -0300

    testes para ver o percentual das reqisicoes

--- CRUD/GET/limites.js
+++ CRUD/GET/limites.js
@@ -10,7 +10,7 @@ export function handleSummary(data) {
 
 export const options = {
   thresholds: {
-    // 90% of requests must finish within 400ms, 95% within 800, and 99.9% within 2s.
+    // 90% das requisicoes finalizadas com 400ms, 95% com 800, and 99.9% com 2s.
     http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'],
   },
 };
