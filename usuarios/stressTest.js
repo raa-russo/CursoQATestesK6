7fe6a2da99e9cce12776c11d839838859c0e5ce0
russo <raa.augusto@hotmail.com>
2023-07-31 17:56:12 -0300

    teste de stress

--- usuarios/stressTest.js
+++ usuarios/stressTest.js
@@ -15,14 +15,14 @@ export function handleSummary(data) {
 
 export const options = {
   stages: [
-    { duration: "2s", target: 100 },
-    { duration: "3s", target: 100 },
-    { duration: "2s", target: 200 },
-    { duration: "3s", target: 200 },
-    { duration: "1s", target: 300 },
-    { duration: "2s", target: 300 },
-    { duration: "3s", target: 400 },
-    { duration: "1s", target: 0 },
+    { duration: "20s", target: 100 },
+    { duration: "30s", target: 100 },
+    { duration: "20s", target: 200 },
+    { duration: "30s", target: 200 },
+    { duration: "10s", target: 300 },
+    { duration: "20s", target: 300 },
+    { duration: "30s", target: 400 },
+    { duration: "10s", target: 0 },
 
   ],
   thresholds: {
