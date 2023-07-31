cdbdf12c4553daf77cea75ac085f116ca522ed23
russo <raa.augusto@hotmail.com>
2023-07-31 12:48:09 -0300

    smoke test login

---
+++ ROTAS/Login/smokeTest.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }  
+
+export default function () {
+    const url = 'http://localhost:3000/login';
+
+    const payload = JSON.stringify({
+        email: 'fulano@qa.com',
+        password: 'teste'
+    })
+    const headers = { 'headers': { 'Content-Type': 'application/json' } }
+    const res = http.post(url, payload, headers) 
+  
+    check(res, {
+        'status shoul be 200': (r) => r.status === 200,        
+    });       
+    console.log(res.body);
+    sleep(1);
+}
+
+
+
+
