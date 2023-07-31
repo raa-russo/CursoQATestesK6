f15ad899913afa0e6666bf5a7b5d8c4a93c47f07
russo <raa.augusto@hotmail.com>
2023-07-31 14:55:52 -0300

    rodar login

---
+++ rodarLogin.js
@@ -0,0 +1,46 @@
+import SmokeTest from "./Login/smokeTest.js";
+import LoadTest from "./Login/loadTest.js";
+import StressTest from "./Login/stressTest.js";
+import StatusCode from "./Login/statusCode.js";
+import { group, sleep } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }
+
+  export const options = {
+    stages: [
+      { duration: '30s', target: 50 },
+      { duration: '2m', target: 100 },
+      { duration: '30s', target: 200 },
+      { duration: '1m', target: 100 },
+      { duration: '3m', target: 0 },
+    ],
+    thresholds: {
+      http_req_duration: ['p(90) < 400', 'p(95) < 800', 'p(99.9) < 2000'], 
+      http_req_failed: ['rate<0.01'] 
+    }
+  }
+
+export default () => {
+    group('Endpoint smoke teste - Serverest.Api', () => {
+      SmokeTest();
+    });
+
+    group('Endpoint load teste - Serverest.Api', () => {
+      LoadTest();
+    });
+
+    group('Endpoint teste de stress - Serverest.Api', () => {
+      StressTest();
+    });
+
+    group('Endpoint teste de status code - Serverest.Api', () => {
+      StatusCode();
+    });
+
+    sleep(1);
+}
