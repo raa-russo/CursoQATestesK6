a2d76780098a71dbf66a25c4ae6e19b260854c0c
russo <raa.augusto@hotmail.com>
2023-08-03 14:39:57 -0300

    loadTest rota usuarios

---
+++ usuarios/loadTestUsers.js
@@ -0,0 +1,65 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import uuid from '../libs/uuid.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+export const options = {
+  stages: [
+    { duration: "1m", target: 100 },
+    { duration: "2m", target: 100 },
+    { duration: "1m", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<2000"],
+    http_req_failed: ['rate<0.01']
+  },
+};
+
+
+export default function () {
+  const url = 'http://localhost:3000/usuarios';
+
+const payload = JSON.stringify ({ 
+  nome: `${uuid.v4()}`, 
+  email: `${uuid.v4().substring(24)}@qa.com`, 
+  password: 'teste', 
+  administrador: 'true' 
+})
+
+const headers = { 'headers': {
+   'Content-Type': 'application/json',
+   'monitor': 'false',
+   } }
+  
+const res = http.post(url, payload, headers)  
+  
+    try {
+      if (res.status >= 200 && res.status < 300) {
+       // console.error('Load Test passou! API connect.');
+      } else {
+       // console.error('Load Test falhou! API returned an error.');
+      }
+    } catch (error) {
+      //console.error('Load Test falhou! Unable to connect to the API.');
+      //console.error(error.message);
+    }
+    check(res, {
+      'status shoul be 201': (r) => r.status === 201,
+      'response should have id': (r) => r.json().hasOwnProperty('_id'),
+    });
+  
+
+  const id = res.json()._id;
+  globalThis.id = id 
+  console.log(res.body)
+  console.log(id)
+  
+  sleep(1)
+}
+
+//k6 run usuarios/loadTestUsers.js
