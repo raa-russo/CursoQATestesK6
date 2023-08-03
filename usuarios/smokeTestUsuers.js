2d333d816046dac68218d80c97daeff85dfe706e
russo <raa.augusto@hotmail.com>
2023-08-03 17:06:07 -0300

    add metricas ao teste

--- usuarios/smokeTestUsuers.js
+++ usuarios/smokeTestUsuers.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { sleep, check, fail } from 'k6';
+import { Trend, Rate, Counter } from 'k6/metrics';
 import uuid from '../libs/uuid.js';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
@@ -19,6 +20,11 @@ export const options = {
   },
 };
 
+export let GetCustomerDuration = new Trend('get_customer_duration');
+export let GetCustomerFailRate = new Rate('get_customer_fail_rate');
+export let GetCustomerSuccessRate = new Rate('get_customer_success_rate');
+export let GetCustomerReqs = new Rate('get_customer_reqs');
+
 export default function () {
   const url = 'http://localhost:3000/usuarios';
 
@@ -36,10 +42,19 @@ const headers = { 'headers': {
   
 const res = http.post(url, payload, headers)
 
-  check(res, {
-    'status shoul be 201': (r) => r.status === 201,
-    'response should have id': (r) => r.json().hasOwnProperty('_id'),   
-  });
+  GetCustomerDuration.add(res.timings.duration);
+  GetCustomerReqs.add(1);
+  GetCustomerFailRate.add(res.status > 200 || res.status > 399);
+  GetCustomerSuccessRate.add(res.status < 399);
+
+  let durationMSG = 'Max Duration ${1000/1000}s'
+  if(!check(res, {
+    'max duration': (r) => r.timings.duration < 2000,
+    'status should be 201': (r) => r.status === 201,
+    'response should have id': (r) => r.json().hasOwnProperty('_id'),
+  })){
+    fail(durationMSG);
+  } 
 
   const id = res.json()._id;
   globalThis.id = id 
