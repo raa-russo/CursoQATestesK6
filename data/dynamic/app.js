ffe41ec5b68806b896348f16699881481e39b64c
russo <raa.augusto@hotmail.com>
2023-08-07 16:06:40 -0300

    tstand faker

---
+++ data/dynamic/app.js
@@ -0,0 +1,12 @@
+const faker = require('faker');
+
+faker.locale = 'pt_BR';
+
+
+console.log(faker.name.fistName(), faker.internet.email(name));
+
+
+//const baseRest = Criação dos métodos;
+//const baseChecks = Criação dos checks;
+
+//thresholds, options, 
